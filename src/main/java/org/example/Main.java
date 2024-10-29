44266a8f77ae18b283d786d5c58c1b8641ce5a87
mrkihiki <mrkihiki@yandex.ru>
2024-10-29 23:28:08 +0700

    l1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,11 +9,26 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Collection collection= new ArrayList<>();
+        //a
+        int N=10;
+        Integer[] array = new Integer[N];
+        for (int i = 0; i < N; ++i) {
+            array[i]=(int) (Math.random() * 101);
         }
+        //b
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list, array);
+        System.out.println(list);
+        //c
+        Collections.sort(list);
+        System.out.println(list);
+        //d
+        Collections.reverse(list);
+        System.out.println(list);
+        //e
+        Collections.shuffle(list);
+        System.out.println(list);
+        //f
     }
 }
